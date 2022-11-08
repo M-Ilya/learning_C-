a8c59f5b270dc2d451381be97d4d2c81b1511a38
Ilya Melnichenko <ilya-melnichenko@yandex.ru>
2022-11-08 22:03:09 +0300

    Решение задачи 25

---
+++ Task_25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+System.Console.WriteLine("Введите число, возводимое с степень: ");
+int number = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите степень числа: ");
+int exponent = int.Parse(Console.ReadLine());
+
+//вызываем функцию
+System.Console.WriteLine(number + " в степени " + exponent + " = " + Expon(number, exponent));
+
+//Функция возведения в степень
+int Expon(int a, int x)
+{
+    int result = 1;
+    for (int i = 0; i < x; i++)
+    {
+        result *= a;
+    }
+    return result;
+}
